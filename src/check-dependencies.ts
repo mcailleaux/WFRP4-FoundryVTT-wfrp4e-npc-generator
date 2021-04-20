641d108396526d37c9cc55e0690ef3b12abd36bc
Matthieu CAILLEAUX <mcailleaux@asi.fr>
2021-04-20 22:16:45 +0200

    feat: allow gen only if needed dependencies are active

--- src/check-dependencies.ts
+++ src/check-dependencies.ts
@@ -3,10 +3,10 @@ export default class CheckDependencies {
     const active = game.modules?.get('wfrp4e-core')?.active ?? false;
     if (active) {
       callback(true);
-    }
-    new Dialog({
-      title: game.i18n.localize('WFRP4NPCGEN.dependencies.miss.title'),
-      content: `<form>
+    } else {
+      new Dialog({
+        title: game.i18n.localize('WFRP4NPCGEN.dependencies.miss.title'),
+        content: `<form>
               <div class="form-group">
               <label>
                   ${game.i18n.localize(
@@ -16,16 +16,17 @@ export default class CheckDependencies {
               <input type="text" readonly value="wfrp4e-core"/>
               </div>
           </form>`,
-      buttons: {
-        yes: {
-          icon: "<i class='fas fa-check'></i>",
-          label: game.i18n.localize('WFRP4NPCGEN.common.button.OK'),
-          callback: () => {
-            callback(false);
+        buttons: {
+          yes: {
+            icon: "<i class='fas fa-check'></i>",
+            label: game.i18n.localize('WFRP4NPCGEN.common.button.OK'),
+            callback: () => {
+              callback(false);
+            },
           },
         },
-      },
-      default: 'yes',
-    }).render(true);
+        default: 'yes',
+      }).render(true);
+    }
   }
 }
