aac75bc2397974b0fd33ae901759417b1b744293
Matthieu CAILLEAUX <mcailleaux@asi.fr>
2021-04-22 17:59:50 +0200

    feat: add random career

--- src/util/career-chooser.ts
+++ src/util/career-chooser.ts
@@ -1,4 +1,5 @@
 import DialogUtil from './dialog-util.js';
+import RandomUtil from './random-util.js';
 
 export default class CareerChooser {
   public static async getCareers(): Promise<Item[]> {
@@ -22,6 +23,12 @@ export default class CareerChooser {
       title: game.i18n.localize('WFRP4NPCGEN.career.select.title'),
       content: `<form>
               <div class="form-group">
+                      ${DialogUtil.getButtonScript(
+                        'WFRP4NPCGEN.common.button.Random',
+                        'random()'
+                      )}
+              </div>
+              <div class="form-group">
               ${DialogUtil.getLabelScript('WFRP4NPCGEN.career.select.label')}
               ${DialogUtil.getInputScript({
                 id: `select-career-${dialogId}`,
@@ -41,6 +48,17 @@ export default class CareerChooser {
                 const yesButton = document.getElementById('yes-icon-${dialogId}').parentElement;
                 yesButton.disabled=!careers.includes(career);
             }
+            
+            function random() {
+              const careers = [${careers.map((c) => `"${c.name}"`).join(',')}]
+              const randomCareer = getRandomValue(careers);
+              if (randomCareer != null) {
+                  document.getElementById('select-career-${dialogId}').value = randomCareer;
+              }
+            }
+              
+            ${RandomUtil.getRandomValueScript()}
+              
             check()
           </script>
           `,
