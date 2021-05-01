f4b4ef87a1a289c90fab2ea7dd567d2c0c90e93d
Matthieu CAILLEAUX <mcailleaux@asi.fr>
2021-05-01 13:44:41 +0200

    feat: init settings generation profiles menu + templates

--- src/util/generation-profiles-form.ts
+++ src/util/generation-profiles-form.ts
@@ -1,3 +1,22 @@
 import GenerationProfiles from './generation-profiles.js';
+import RegisterSettings from './register-settings.js';
 
-export default class GenerationProfilesForm extends FormApplication<GenerationProfiles> {}
+export default class GenerationProfilesForm extends FormApplication<GenerationProfiles> {
+  constructor(object: GenerationProfiles, options: FormApplication.Options) {
+    super(object, options);
+  }
+
+  public static get defaultOptions(): FormApplication.Options {
+    return mergeObject(super.defaultOptions, {
+      id: 'generation-profiles',
+      title: game.i18n.localize('WFRP4NPCGEN.settings.generationProfiles.name'),
+      template: `modules/${RegisterSettings.moduleName}/templates/generation-profiles.html`,
+      width: 750,
+      height: 'auto',
+      top: 200,
+      left: 400,
+      resizable: true,
+      closeOnSubmit: false,
+    });
+  }
+}
